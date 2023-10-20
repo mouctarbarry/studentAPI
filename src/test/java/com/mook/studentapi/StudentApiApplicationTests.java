26522cddb438830d6840325836678c91690edf11
mouctarbarry <mouctar.barry@sogeti.com>
2023-10-20 21:00:33 +0200

    initial commit

---
+++ src/test/java/com/mook/studentapi/StudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mook.studentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
